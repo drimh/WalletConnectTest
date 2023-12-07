1c3751a7792bd6497b7deb5c4c5d1e3637b84a1d
drimh <107299318+drimh@users.noreply.github.com>
2023-12-07 20:11:48 +0900

    feat:setting

---
+++ WallectConnectTest/src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { walletConnectProvider } from '@web3modal/wagmi'
+import { WagmiConfig, configureChains, createConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { mainnet } from 'viem/chains'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+
+const projectId = import.meta.env.VITE_PROJECT_ID; 
+export const apikey = import.meta.env.VITE_ALCHEMY_KEY;
+
+const { chains, publicClient } = configureChains(
+  [mainnet],
+  [walletConnectProvider({ projectId }), publicProvider()]
+)
+
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [
+    new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+  ],
+  publicClient
+})
+
+createWeb3Modal({ wagmiConfig, projectId, chains })
+
+function App() {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <w3m-button />
+    </WagmiConfig>
+  )
+}
+
+export default App
