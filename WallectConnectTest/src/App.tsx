49edc3716eb820fd19d7e6229fad5d735356f4a7
drimh <107299318+drimh@users.noreply.github.com>
2023-12-08 13:24:24 +0900

    feat: walletConnect UI

--- WallectConnectTest/src/App.tsx
+++ WallectConnectTest/src/App.tsx
@@ -6,9 +6,11 @@ import { publicProvider } from 'wagmi/providers/public'
 import { mainnet } from 'viem/chains'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import ConnectButton from '../components/ConnectButton'; 
+import purifyLogo from '../image/purifyLogo.png'; 
 
 const projectId = import.meta.env.VITE_PROJECT_ID; 
 export const apikey = import.meta.env.VITE_ALCHEMY_KEY;
+
 
 const { chains, publicClient } = configureChains(
   [mainnet],
@@ -36,16 +38,38 @@ createWeb3Modal({
   chains,
   themeVariables: {
     '--w3m-accent': '#06F', 
-    '--w3m-border-radius-master': '1.25rem', 
-    '--w3m-font-family':'Poppins, --w3m-text-medium-regular-size, --w3m-text-xsmall-regular-line-height'
+    '--w3m-border-radius-master': '1.25rem'
   }
 })
 
+const connectWallectBox = { 
+  backgroundColor: 'skyblue', //구분용 임시 색상
+  width:'16.875rem', 
+  height:'28.125rem'
+}
+
+const logoImg={
+  marginTop:'0.62rem',
+  marginBottom:'2.25rem',
+  color:'#444', 
+  fontFamily:"Audiowide",
+  fontSize:'1.5rem', 
+  fontStyle:'normal', 
+  fontWeight:'400', 
+  lineHeight:'normal'
+} 
+
 function App() {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <ConnectButton/>
-    </WagmiConfig>
+    <div style={connectWallectBox}>
+      <img src={purifyLogo} style={{marginTop:'5.56rem'}}/>
+      <p style={logoImg}>purify</p>
+      <div style={{display:'flex', justifyContent:'center'}}>
+        <WagmiConfig config={wagmiConfig}>
+          <ConnectButton/>
+        </WagmiConfig>
+      </div>
+    </div>
   )
 }
 
